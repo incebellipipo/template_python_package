0917590bdbe5782cd5627ee198b9b6426997acce
Emir Cem Gezer <emircem.gezer@gmail.com>
2023-08-03 15:33:14 +0200

    initial commit

---
+++ src/template_package/__init__.py
@@ -0,0 +1,4 @@
+"""Template Package"""
+from __future__ import annotations
+
+__version__ = "0.0.1"
---
+++ src/template_package/hi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Doc String."""
+
+from __future__ import annotations
+
+
+def template_function(i: int = 0) -> str:
+    """Doc String."""
+    return f"string-{i}"
